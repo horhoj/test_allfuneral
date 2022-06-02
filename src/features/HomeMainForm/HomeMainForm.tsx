2bc45ff4fb6d6abf7cad6a27615d9fb074ee64d0
cool29 <cool29@yandex.ru>
2022-06-02 09:00:45 +0500

    init

---
+++ src/features/HomeMainForm/HomeMainForm.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import styles from './HomeMainForm.module.scss';
+
+export const HomeMainForm: FC = () => {
+  return (
+    <div className={styles.wrap}>
+      <div>Home</div>
+    </div>
+  );
+};
